7656b467c6d7f1cc377d8eff7016271522e44b78
ariccio3 <ar3home@gmail.com>
2017-09-12 06:32:02 -0700

    minor updates to user.js

--- models/user.js
+++ models/user.js
@@ -1,59 +1,60 @@
 module.exports = function(sequelize, DataTypes) {
-    var Members = sequelize.define("Table", {
+  var Members = sequelize.define("Members", {
     id: {
-        type: DataTypes.INTERGER,
-        autoIncrement: true,
-        primaryKey: true,
-        min: 1000
-        },
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-        },
-      type: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      gym: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      min: 1000
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
         len: [1]
-        }
-      },
-      zipCode: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate:{
-        len: [5]
-        }
-      },
-      month: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      day: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      time: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      am_pm: {
-        type: DataTypes.STRING,
-        allowNull: false
       }
-    });
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    userType: {
+       type: DataTypes.STRING,
+       allowNull: false
+    },
+    gym: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+      len: [1]
+      }
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true,
+      validate:{
+      len: [5]
+      }
+    },
+    month: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    day: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    am_pm: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
     return Members;
-  };
+};

7f7c827d58d5e3a01050e24244763ea238f7020b
Joseph Calabrese <j.calabrese091@gmail.com>
2017-09-13 14:55:11 -0700

    update .js

---
+++ models/expert.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+  var Expert = sequelize.define("Expert", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // validate: {
+      //   len: [1]
+      // }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1]
+      }
+    },
+    userType: {
+       type: DataTypes.STRING,
+       allowNull: true
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    gym: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+      timestamps: false    
+  });
+
+    return Expert;
+};
---
+++ models/rookie.js
@@ -0,0 +1,42 @@
+module.exports = function(sequelize, DataTypes) {
+  var Rookie = sequelize.define("Rookie", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // validate: {
+      //   len: [1]
+      // }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [1]
+      }
+    },
+    userType: {
+       type: DataTypes.STRING,
+       allowNull: true
+    },
+    photo: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    gym: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  }, {
+      timestamps: false    
+  });
+
+    return Rookie;
+};

10891e00450bd1b411505ec812bf79667e4e5a54
Joseph Calabrese <j.calabrese091@gmail.com>
2017-09-13 11:10:57 -0700

    update reservation.js

---
+++ public/js/reservation.js
@@ -0,0 +1,28 @@
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    var newReservation =  {
+      userName: $(".userNameField")
+      gym: $(".gymField").val().trim(), 
+      month: $(".monthField").val().trim(),
+      day: $(".dayField").val().trim(),
+      am_pm: $(".am_pmField").val().trim(),
+      hour: $(".hourField").val().trim()
+    };
+      
+    console.log(newReservation);
+    
+    $.put("/setappointment", newReservation)
+
+     .done(function(data) {
+        console.log(data);
+
+    });
+      $(".userNameField").val("");
+      $(".gymField").val("");
+      $(".monthField").val("");
+      $(".dayField").val("");
+      $(".am_pmField").val("");
+      $(".hourField").val("")
+    };
+  });
+

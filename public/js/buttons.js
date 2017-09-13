5e8244621c633c669ad5b30856571c863bed27a6
Tori White <toriw22@gmail.com>
2017-09-13 12:01:24 -0700

    fixed background photos so don't conflict with map

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,26 +1,26 @@
-		$(".loginInput").hide();
-		$(".beginnerOrPro").hide();
-		$(".loginButton").on("click", function(event){
-		   	$(".newUserButton").hide();
-		   	$(".loginInput").show();
-		   	$(".newUserOrLogin").addClass("login");
-		   	$(".login").removeClass("newUserOrLogin");
-		   	$(".loginButton").addClass("newLoginButton");
-		   	var expertRadio = document.getElementById("expert");
-	 		var rookieRadio = document.getElementById("rookie");
-	 		
-	 		if (rookieRadio.checked) {
-	 			console.log("rookie radio checked!");
-	 			$(".loginButton").attr("href", "user.html");
-			}
-	 		if (expertRadio.checked) {
-	 			console.log("expert radio checked!");
-	 			$(".loginButton").attr("href", "pro.html");
-	 		}
-	 	});
-	 	$(".newUserButton").on("click", function(event){
-	    	event.preventDefault();
-		   	$(".newUserButton").hide();
-		   	$(".loginButton").hide();
-		   	$(".beginnerOrPro").show();
-	 	});
+$(".loginInput").hide();
+$(".beginnerOrPro").hide();
+$(".loginButton").on("click", function(event){
+   	$(".newUserButton").hide();
+   	$(".loginInput").show();
+   	$(".newUserOrLogin").addClass("login");
+   	$(".login").removeClass("newUserOrLogin");
+   	$(".loginButton").addClass("newLoginButton");
+   	var expertRadio = document.getElementById("expert");
+	var rookieRadio = document.getElementById("rookie");
+		
+	if (rookieRadio.checked) {
+		console.log("rookie radio checked!");
+		$(".loginButton").attr("href", "user.html");
+	}
+	if (expertRadio.checked) {
+		console.log("expert radio checked!");
+		$(".loginButton").attr("href", "pro.html");
+	}
+});
+$(".newUserButton").on("click", function(event){
+	event.preventDefault();
+   	$(".newUserButton").hide();
+   	$(".loginButton").hide();
+   	$(".beginnerOrPro").show();
+});

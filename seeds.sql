ced944891072cd1d9927754bdaf9022a9103fbf6
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-12 21:52:05 -0700

    Added anthony's updated seeds data to seeds.sql and anthony's jQuery code to form.html, updated server.js require to api-routes.js

--- seeds.sql
+++ seeds.sql
@@ -1,12 +1,29 @@
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Henry", "Davis", "hdavis", "User", "24 Hour Fitness", 92629, "September", "15","3", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Jessica", "Garcia", "jgarcia", "Trainer", "LA Fitness", 92663, "September", "15","3", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Louis", "Smith", "lsmith", "User", "24 Hour Fitness", 90007, "September", "15","3", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Rhonda", "Lee", "rlee", "User", "LA Fitness", 92646, "September", "16","6", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Zeke", "Douglas", "zdouglas", "Trainer", "24 Hour Fitness", 92629, "September", "15","5", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Ronald", "Nicholas", "rnicholas", "Trainer", "LA Fitness", 92663, "September", "16","2", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Memphis", "Brown", "mbrown", "User", "24 Hour Fitness", 90007, "September", "15","10", "AM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Stephanie", "Gonzales", "sgonzales", "Trainer", "LA Fitness", 92646, "September", "13","5", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Larry", "Johnson", "ljohnson", "User", "24 Hour Fitness", 92629, "September", "17","11", "AM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("May", "Raymond", "mraymond", "Trainer", "LA Fitness", 92663, "September", "14","4", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Peter", "Philips", "pphilips", "User", "24 Hour Fitness", 90007, "September", "17","3", "PM");
-INSERT INTO Members (firstName, lastName, userName, type, gym, zipCode, month, day, time, am_pm) VALUES ("Amy", "Ericson", "aericson", "User", "LA Fitness", 92646, "September", "16","7", "PM");
+use gym_db;
+
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Henry", "Davis", "hdavis", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Jessica", "Garcia", "jgarcia", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Louis", "Smith", "lsmith", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Rhonda", "Lee", "rlee", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","6", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Zeke", "Douglas", "zdouglas", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","5", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Ronald", "Nicholas", "rnicholas", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","2", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Memphis", "Brown", "mbrown", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","10", "AM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Stephanie", "Gonzales", "sgonzales", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "13","5", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Larry", "Johnson", "ljohnson", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "17","11", "AM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("May", "Raymond", "mraymond", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "14","4", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Peter", "Philips", "pphilips", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "17","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Amy", "Ericson", "aericson", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","7", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Daisy", "Davis", "ddavis", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Erica", "Garcia", "egarcia", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Rose", "Smith", "rsmith", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Henry", "Lee", "hlee", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","6", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Bella", "Douglas", "bdouglas", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","5", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Sophia", "Nicholas", "snicholas", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","2", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Dana", "Brown", "dbrown", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "15","10", "AM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Morgan", "Gonzales", "mgonzales", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "13","5", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Brian", "Johnson", "bjohnson", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "17","11", "AM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Tom", "Raymond", "traymond", "Expert", "http://via.placeholder.com/150x150", "LA Fitness", "9", "14","4", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Ellen", "Philips", "ephilips", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness", "9", "17","3", "PM");
+INSERT INTO Members (firstName, lastName, userName, userType, photo, gym, month, day, time, am_pm) VALUES ("Patty", "Ericson", "pericson", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness", "9", "16","7", "PM");
+
+
+SELECT * FROM gym_db.members;

11b703dd5509c92aab258058e5812e95af8b3de2
Joseph Calabrese <j.calabrese091@gmail.com>
2017-09-13 00:17:29 -0700

    update pro_form.js

---
+++ public/js/pro_form.js
@@ -0,0 +1,23 @@
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    var newRookie =  {
+      firstName: $(".firstNameField").val().trim(), 
+      lastName: $(".lastNameField").val().trim(),
+      userName: $(".userNameField").val().trim(),
+      photo: $(".photoField").val().trim(),
+      userType: "expert"
+    };
+      
+    console.log(newRookie);
+    
+    $.post("/newUser", newRookie)
+
+     .done(function(data) {
+        console.log(data);
+
+    });
+      $(".firstNameField").val("");
+      $(".lastNameField").val("");
+      $(".userNameField").val("");
+      $(".photoField").val("");
+  });

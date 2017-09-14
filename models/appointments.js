4a189eda6abb2658b20f5c0fbea8dca2cddfe088
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-14 12:53:56 -0700

    added appointments seeds added new models for expert, rookie, and appointment. Added map to form.html (rookie form)

--- models/appointments.js
+++ models/appointments.js
@@ -1,29 +1,53 @@
 module.exports = function(sequelize, DataTypes) {
-    var Appointments = sequelize.define("Appointments", {
-        month: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      day: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      time: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      am_pm: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
-  }, {
-        timestamps: false    
+  var Appointments = sequelize.define("Appointments", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    month: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    day: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    time: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    am_pm: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    gym: {
+    type: DataTypes.STRING,
+    allowNull: false
+    }
+}, {
+      timestamps: false    
+  });
+
+  Appointments.associate = function(models) {
+    models.Rookie.belongsToMany(models.Expert, { 
+      through: Appointments 
+    }, { 
+      foreignKey: { 
+        allowNull: true 
+      }, 
+      onDelete: 'CASCADE' 
     });
   
-    Appointments.associate = function(models) {
-      models.Rookie.belongsToMany(models.Expert, { through: Appointments } );
-      models.Expert.belongsToMany(models.Rookie, { through: Appointments } );
-    };
-  
-      return Appointments;
+    models.Expert.belongsToMany(models.Rookie, { 
+      through: Appointments 
+    }, { 
+      foreignKey: { 
+        allowNull: true 
+      }, 
+      onDelete: 'CASCADE' 
+    }); 
   };
+
+    return Appointments;
+};

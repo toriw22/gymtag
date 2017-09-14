f7d0dd12792b792ad737eac608cb695f0ff364c3
Garrett Moore <garrettmarkmoore@gmail.com>
2017-09-13 18:07:06 -0700

    added expert and rookie seeds files that anthony posted in slack

---
+++ expert-seeds.sql
@@ -0,0 +1,29 @@
+use gym_db;
+
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Ralph", "Davis", "rdavis", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Jessica", "Garcia", "jgarcia", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Pei", "Smith", "psmith", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Piper", "Lee", "plee", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Zeke", "Douglas", "zdouglas", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Ronald", "Nicholas", "rnicholas", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Sacagawea", "Brown", "sbrown", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Stephanie", "Gonzales", "sgonzales", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Kim", "Johnson", "kjohnson", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("May", "Raymond", "mraymond", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Amy", "Philips", "aphilips", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Scott", "Ericson", "sericson", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Judy", "Davis", "jdavis", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Erica", "Garcia", "egarcia", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Hilda", "Smith", "hsmith", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Spike", "Lee", "slee", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Bella", "Douglas", "bdouglas", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Sophia", "Nicholas", "snicholas", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Rhonda", "Brown", "rbrown", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Morgan", "Gonzales", "mgonzales", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Don", "Johnson", "djohnson", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Tom", "Raymond", "traymond", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Jenna", "Philips", "jphilips", "Expert", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Experts (firstName, lastName, userName, userType, photo, gym) VALUES ("Sally", "Ericson", "sericson", "Expert", "http://via.placeholder.com/150x150", "LA Fitness");
+
+
+SELECT * FROM gym_db.Experts;
---
+++ rookie-seeds.sql
@@ -0,0 +1,29 @@
+use gym_db;
+
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Henry", "Davis", "hdavis", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Ester", "Garcia", "egarcia", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Louis", "Smith", "lsmith", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Rhonda", "Lee", "rlee", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Beatrice", "Douglas", "bdouglas", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Alabama", "Nicholas", "anicholas", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Roger", "Brown", "rbrown", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Yanna", "Gonzales", "ygonzales", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Larry", "Johnson", "ljohnson", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Steph", "Raymond", "mraymond", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Peter", "Philips", "pphilips", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Amy", "Ericson", "aericson", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Daisy", "Davis", "ddavis", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Aiesha", "Garcia", "agarcia", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Rose", "Smith", "rsmith", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Henry", "Lee", "hlee", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("James", "Douglas", "jdouglas", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Nicole", "Nicholas", "nnicholas", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Morgan", "Brown", "bbrown", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Tony", "Gonzales", "tgonzales", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Brian", "Johnson", "bjohnson", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Joe", "Raymond", "jraymond", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Ellen", "Philips", "ephilips", "Rookie", "http://via.placeholder.com/150x150", "24 Hour Fitness");
+INSERT INTO Rookies (firstName, lastName, userName, userType, photo, gym) VALUES ("Patty", "Ericson", "pericson", "Rookie", "http://via.placeholder.com/150x150", "LA Fitness");
+
+
+SELECT * FROM gym_db.Rookies;
